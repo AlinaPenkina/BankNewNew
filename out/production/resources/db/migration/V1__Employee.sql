772ca97337ccc777a6a39243cc423217c14ee9cd
apenkina <1303198728032013>
2019-09-05 13:27:25 +0600

    first commit

---
+++ out/production/resources/db/migration/V1__Employee.sql
@@ -0,0 +1,8 @@
+ CREATE TABLE employee1 (
+   id serial PRIMARY KEY,
+   first_name VARCHAR(100),
+   last_name VARCHAR(100),
+   amount INTEGER,
+   price DECIMAL,
+   category VARCHAR (100)
+);
---
+++ src/main/java/com/example/demo/worker/Employee.java
@@ -0,0 +1,73 @@
+package com.example.demo.worker;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "employee1")
+public class Employee {
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private Integer amount;
+    private Integer price;
+    private String category;
+
+    public Employee(){
+
+    }
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(){
+        this.lastName = lastName;
+    }
+
+    public Integer getAmount(){
+        return amount;
+    }
+
+    public void setAmount(){
+        this.amount = amount;
+    }
+
+    public Integer getPrice(){
+        return price;
+    }
+
+    public void setPrice(){
+        this.price = price;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public void setCategory(){
+        this.category = category;
+    }
+
+
+
+}
